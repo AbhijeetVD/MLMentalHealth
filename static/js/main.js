deca075c21f46aba7274ea9f9433c5b5f254c20f
Abhijeet Deshpande <deshpande007123@gmail.com>
2023-05-07 23:52:31 +0530

    2

--- static/js/main.js
+++ static/js/main.js
@@ -1,32 +1,3 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>0)
 })
-const quoteText = document.querySelector(".quote"),
-quoteBtn = document.querySelector("#btn"),
-authorName = document.querySelector(".name"),
-speechBtn = document.querySelector(".speech"),
-copyBtn = document.querySelector(".copy"),
-synth = speechSynthesis;
-function randomQuote(){
-    quoteBtn.classList.add("loading");
-    quoteBtn.innerText = "Loading Quote...";
-    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").then(response => response.json()).then(result => {
-        quoteText.innerText = result.content;
-        authorName.innerText = result.author;
-        quoteBtn.classList.remove("loading");
-        quoteBtn.innerText = "New Quote";
-    });
-}
-speechBtn.addEventListener("click", ()=>{
-    if(!quoteBtn.classList.contains("loading")){
-        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
-        synth.speak(utterance);
-        setInterval(()=>{
-            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
-        }, 10);
-    }
-});
-copyBtn.addEventListener("click", ()=>{
-    navigator.clipboard.writeText(quoteText.innerText);
-});
-quoteBtn.addEventListener("click", randomQuote);
